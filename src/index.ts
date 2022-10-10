35a9546cffab405132e060ca8c30517aa586fea1
Marcos Peter <mpeterlobato@gmail.com>
2022-10-10 14:18:25 -0300

    [Code Review Fix] Removing code comments

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,3 @@
-// import { ApolloServer, gql } from 'apollo-server';
-// import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-
-// const typeDefs = gql `
-//   type Query {
-//     hello: String
-//   }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     hello: () => 'This is a Hello World! from a GraphQL API configured with Apollo Server',
-//   }
-// };
-
-// const server = new ApolloServer ({
-//   typeDefs,
-//   resolvers,
-//   csrfPrevention: true,
-//   cache: 'bounded',
-//   plugins: [
-//     ApolloServerPluginLandingPageLocalDefault({ embed: true })
-//   ],
-// });
-
-// server.listen().then(
-//   ({ url }) => console.log(`[SERVER] - Server running at ${url}`)
-// );
-
 import { AppDataSource } from './data-source';
 import { User } from './entity/User';
 import { apolloServerRun } from './apolloServer';
