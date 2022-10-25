215b56b508b89d659d2a6044b81be9e544852aa8
Marcos Peter <mpeterlobato@gmail.com>
2022-10-24 21:53:07 -0300

    Login tests with token

--- src/utils/authenticate.auth.ts
+++ src/utils/authenticate.auth.ts
@@ -15,21 +15,29 @@ export async function authenticateUser(args) {
   const passwordMatch = await crypt.isEqual(args.login.password, user.password);
 
   if (!passwordMatch) {
-    throw new CustomError('Invalid credentials. Wrong email or password. ', 401);
+    throw new CustomError('Invalid credentials. Wrong email or password.', 401);
   }
 
   if (args.login.rememberMe) {
-    const token = sign({ id: user.id, name: user.name, email: user.email }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_REMEMBER_ME,
-    });
+    const token = sign(
+      { iss: 'onboard-marcos-peter-API', sub: { id: user.id, name: user.name, email: user.email } },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_REMEMBER_ME,
+      },
+    );
     console.info(`[SERVER] - User logged in: ${user.name}`);
 
     return { user, token };
   }
 
-  const token = sign({ id: user.id, name: user.name, email: user.email }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = sign(
+    { iss: 'onboard-marcos-peter-API', sub: { id: user.id, name: user.name, email: user.email } },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+  );
 
   console.info(`[SERVER] - User logged in: ${user.name}`);
   return { user, token };
